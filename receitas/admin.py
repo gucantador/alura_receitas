e8de8a4f055f9e1e10f0e01d5dcba916f18c5636
Gustavo Cantador <grgcantador@gmail.com>
2022-08-23 16:24:04 -0300

    bd funcionando sem cruzar pessoa/receita

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria') #mostra no admin 
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ['nome_receita',]
+    list_filter = ('categoria',)
+
+admin.site.register(Receita, ListandoReceitas) #tem que passar aqui a classe do que voce quer mostrar no admin
